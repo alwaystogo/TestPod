5010e907f36f80491584e1a1615f7b63af9c826c
yunfei.yang <623099552@qq.com>
2018-10-09 10:55:36 +0800

    Initial Commit

---
+++ UIViewCategary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewCategary
+//
+//  Created by yunfei.yang on 2018/10/9.
+//  Copyright © 2018年 yunfei.yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
